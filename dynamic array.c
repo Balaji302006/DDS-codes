59221f7b6ac224257453b5575abdc6a8a80b6698
Balaji302006 <kudupudibalaji2006@gmail.com>
2025-07-01 08:31:39 +0530

    Create dynamic array.c

---
+++ dynamic array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int n;
+    printf("enter the size of array: \n");
+    int* arr=(int*)malloc(n*sizeof(int));
+    for(int i=0; i<=4; i++){
+        printf("enter the value at arr[%d]index: ",i);
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0; i<=4; i++){
+        printf("%d",arr[i]);
+    }
+}
